2b58e174192a6b756a3bc778d79c25b2c717472a
Maciej Opaliński <poprostumieciek@gmail.com>
2022-12-04 13:45:11 +0100

    feat: Add `2022/day_4/`

---
+++ 2022/day_4/part_2.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <vector>
+#include <array>
+using namespace std;
+
+bool is_in_range(int t, int x, int y)
+{
+    return x <= t && t <= y;
+}
+
+bool does_overlap(int x1, int y1, int x2, int y2)
+{
+    return is_in_range(x1, x2, y2) || is_in_range(y1, x2, y2) || is_in_range(x2, x1, y1) || is_in_range(y2, x1, y1);
+}
+
+int main()
+{
+    size_t count = 0;
+
+    int x1, y1, x2, y2;
+    while (scanf("%d-%d,%d-%d\n", &x1, &y1, &x2, &y2) != EOF)
+    {
+        count += does_overlap(x1, y1, x2, y2);
+    }
+
+    printf("%d\n", count);
+}
